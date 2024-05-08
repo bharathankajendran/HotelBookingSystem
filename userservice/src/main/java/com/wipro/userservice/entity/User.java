a7b803ac6f8ff144bd30bccfd7786f6b19de368b
bharathankajendran <149857080+bharathankajendran@users.noreply.github.com>
2024-05-08 11:08:01 +0530

    Update User.java

--- userservice/src/main/java/com/wipro/userservice/entity/User.java
+++ userservice/src/main/java/com/wipro/userservice/entity/User.java
@@ -4,19 +4,29 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name="user_tbl")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int userId;
-
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String userName;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+    @NotBlank(message = "Password is required")
     private String password;
+    @NotBlank(message = "Role is required")
     private String role;
+    @Pattern(regexp="(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobile;
     private String address;
 	public int getUserId() {
@@ -61,7 +71,5 @@ public class User {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	public User() {
-		super();
-	}
+	
 }
